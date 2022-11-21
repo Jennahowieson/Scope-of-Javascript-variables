76cb20f62d8db71acbf662b43a761bd7f6a23357
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-11-21 23:20:13 +0000

    HW Answers

---
+++ homework_answers.js
@@ -0,0 +1,11 @@
+// Miss Scarlet will be the murderer because the function Declare Murderer is on the same level as the Const 'scenario' so it can access the contents
+
+// Error because you can't redifine a const variable.
+
+// First Mrs Peacock, second Professor Plum
+
+// Colenal Mustard, because let can be updated and declareAllSuspects is within the scope to change suspect 3 <- wrong
+
+// Mrs White because changeMurderer calls two functions and 'plot twist' s called second.
+
+// Mr. Green because Const changes the Let, but the plot twist is out of scope within a block
